02c29ccfab11a8eb27eed13b4fa4f87c654d7df3
Fred Schauß <vrkknn@vrkknn.net>
2023-02-07 00:05:01 -0300

    changes

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 termux-change-repo
+pkg upgrade -y
+pkg install -y micro lsd neofetch 
 
 git clone https://github.com/Virkkunen/termux.git "$HOME/termux-vrkknn"
 mv "$HOME/termux-vrkknn/colors"/* "$HOME/.termux/colors"
@@ -11,8 +13,9 @@ echo "alias ls='lsd'" >> "$HOME/.zshrc"
 echo "alias ll='ls -hl'" >> "$HOME/.zshrc"
 echo "alias la='ls -hal'" >> "$HOME/.zshrc"
 
-pkg upgrade -y
-pkg install -y micro lsd neofetch 
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+cp -v "$HOME/zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh" "$HOME/.zsh/"
+echo "source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 
 echo "oh-my-zsh install complete!\nChoose your color scheme now"
 $HOME/.termux/colors.sh
