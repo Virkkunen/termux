6e50f905ba6964fe39334482fddad8c5f20c570f
Fred Almeida <vrkknn@vrkknn.net>
2023-02-07 00:46:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,9 +21,15 @@ sed -i '$d' "$HOME/.zshrc"
 echo "source ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 
+git clone https://github.com/catppuccin/micro.git
+mkdir -p ~/.config/micro/colorschemes
+mv "~/micro/src"/* "~/.config/micro/colorschemes"
+echo "export 'MICRO_TRUECOLOR=1'" >> "$HOME/.zshrc"
+
 rm -rf termux-vrkknn
 rm -rf termux-ohmyzsh
 rm -rf zsh-syntax-highlighting
+rm -rf micro
 touch .hushlogin
 
 echo "addons install complete!\nChoose your color scheme now"
