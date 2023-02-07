09d15bc47cf8b6dbe838c25d5a8508e5028450f6
Fred Almeida <vrkknn@vrkknn.net>
2023-02-06 23:43:56 -0300

    remove bat

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
-pkg install -y micro lsd bat neofetch
+pkg install -y micro lsd neofetch
 
 echo "alias cls='clear'" >> "$HOME/.zshrc"
 echo "alias fetch='clear && neofetch'" >> "$HOME/.zshrc"
 echo "alias ls='lsd'" >> "$HOME/.zshrc"
 echo "alias ll='ls -hl'" >> "$HOME/.zshrc"
 echo "alias la='ls -hal'" >> "$HOME/.zshrc"
-echo "alias cat='bat'" >> "$HOME/.zshrc"
 
 git clone https://github.com/Virkkunen/termux.git "$HOME/termux-vrkknn"
 mv "$HOME/termux-vrkknn/colors"/* "$HOME/.termux/colors"
