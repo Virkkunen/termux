849d420e70ca8943e3f12d75b21472d3be30525d
Fred Schauß <vrkknn@vrkknn.net>
2023-02-06 23:49:14 -0300

    back to full installation

--- install.sh
+++ install.sh
@@ -1,11 +1,41 @@
-pkg install -y micro lsd neofetch
+#!/data/data/com.termux/files/usr/bin/bash
+termux-setup-storage
 
+pkg update
+pkg install -y git zsh micro lsd neofetch
+git clone https://github.com/Virkkunen/termux.git "$HOME/termux-vrkknn" --depth 1
+
+mv "$HOME/.termux" "$HOME/.termux.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp -R "$HOME/termux-vrkknn/.termux" "$HOME/.termux"
+
+git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" --depth 1
+mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+sed -i '/^ZSH_THEME/d' "$HOME/.zshrc"
+sed -i '1iZSH_THEME="agnoster"' "$HOME/.zshrc"
+echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.zshrc"
+echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.zshrc"
 echo "alias cls='clear'" >> "$HOME/.zshrc"
 echo "alias fetch='clear && neofetch'" >> "$HOME/.zshrc"
 echo "alias ls='lsd'" >> "$HOME/.zshrc"
 echo "alias ll='ls -hl'" >> "$HOME/.zshrc"
 echo "alias la='ls -hal'" >> "$HOME/.zshrc"
 
-git clone https://github.com/Virkkunen/termux.git "$HOME/termux-vrkknn"
-mv "$HOME/termux-vrkknn/colors"/* "$HOME/.termux/colors"
-mv "$HOME/termux-vrkknn/fonts"/* "$HOME/.termux/fonts"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+chsh -s zsh
+
+echo "oh-my-zsh install complete!\nChoose your color scheme now"
+$HOME/.termux/colors.sh
+
+echo "Choose your font now"
+$HOME/.termux/fonts.sh
+
+echo "Please restart Termux app..."
+
+exit
+
+# git clone https://github.com/Virkkunen/termux.git "$HOME/termux-vrkknn"
+# mv "$HOME/termux-vrkknn/colors"/* "$HOME/.termux/colors"
+# mv "$HOME/termux-vrkknn/fonts"/* "$HOME/.termux/fonts"
